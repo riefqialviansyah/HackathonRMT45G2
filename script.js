b9a489244ed64f8a091d65e70d0d9318a9247548
balanced-B <ariobimo.mp@gmail.com>
2023-12-20 15:47:03 +0700

    template awal

---
+++ script.js
@@ -0,0 +1,7 @@
+let list = [
+    {image: 'https://asset-2.tstatic.net/tribunnews/foto/bank/images/thanos-dan-gauntlet-6-infinity-stones.jpg', pertanyaan: 'Kenapa Thanos tidak punya rambut?', jawaban: ['Supaya muat pake helm', 'Karena sudah takdir', 'Karena sudah sangat tua']},
+    {image: '', pertanyaan: 'Apa nama belakang Tony kalau sudah sakit?', jawaban: ['Tony Stroke', 'Tetap Tony Stark', '']},
+    {image: '', pertanyaan: '', jawaban: []},
+    {image: '', pertanyaan: '', jawaban: []},
+    {image: '', pertanyaan: '', jawaban: []},
+]
