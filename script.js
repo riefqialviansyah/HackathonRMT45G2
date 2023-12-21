c7c0aa261855e495aae05fc8d8cd822273706628
Aryo Baskoro Setiawan <aryobaskorosetiawan@gmail.com>
2023-12-22 06:11:00 +0700

    Final2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-if(document.title !== 'Highscores'){
+if (document.title !== "Highscores") {
   const list = [
     {
       id: 1,
@@ -21,7 +21,8 @@ if(document.title !== 'Highscores'){
     {
       id: 3,
       image: "IMAGES/jet.jpeg",
-      pertanyaan: "Ketika kapal tenggelam atau pesawat jatuh, munculnya dimana?",
+      pertanyaan:
+        "Ketika kapal tenggelam atau pesawat jatuh, munculnya dimana?",
       pilihanJawaban: [
         "di dasar laut dan di daratan",
         "jawaban atas dan bawah salah",
@@ -45,19 +46,18 @@ if(document.title !== 'Highscores'){
       jawabanBenar: "Kolplay",
     },
   ];
-  
+
   // New
   const optionA = document.getElementById("optionA");
   const optionB = document.getElementById("optionB");
   const optionC = document.getElementById("optionC");
   const gambarBox = document.getElementById("gambarBox");
   const pertanyaanBox = document.getElementById("pertanyaanBox");
-  
-  
+
   let counterPertanyaan = 0;
   let counterSoal = 0;
   let score = 0;
-  
+
   /*
     {
       id: 5,
@@ -67,125 +67,131 @@ if(document.title !== 'Highscores'){
       jawabanBenar: "Kolplay",
     }
   */
-  
-  function gantiSoal(){
-      let perSoal = list[counterPertanyaan];
-      let {image,pertanyaan,pilihanJawaban} = perSoal;
-  
-      gambarBox.setAttribute("src", image)
-      optionA.innerHTML = pilihanJawaban[0]
-      optionB.innerHTML = pilihanJawaban[1]
-      optionC.innerHTML = pilihanJawaban[2]
-      pertanyaanBox.innerHTML = pertanyaan;
-  
-      if(counterPertanyaan==list.length-1){
-          counterPertanyaan = 0;
-      }else{
-          counterPertanyaan++
-      };
-      counterSoal++
-  };         
-  
-  function cekJawaban(soal,userJawaban,list){
-      for(let i=0 ; i<list.length ; i++){
-          let perSoal = list[i];
-          // console.log(perSoal.jawabanBenar,userJawaban,'<<<<<')
-          if(perSoal.pertanyaan==soal){
-              if(perSoal.jawabanBenar==userJawaban){
-                  score += 10
-              }
-          }
-      }
-  }
-  
-  let jawabanBox = document.getElementById("jawabanBox");
-  jawabanBox.addEventListener("click", function(el){
-      if(el.target.id=="optionA" || el.target.id=="optionB" || el.target.id=="optionC"){
-          let jawaban = ''
-          let soal = pertanyaanBox.innerHTML;
-          if(el.target.id=="optionA"){
-              jawaban = optionA.innerHTML;
-          }else if(el.target.id=="optionB"){
-              jawaban = optionB.innerHTML;
-          }else if(el.target.id=="optionC"){
-              jawaban = optionC.innerHTML;
-          }
-          console.log(soal,jawaban)
-          cekJawaban(soal,jawaban,list)
-          console.log(score,counterSoal,'<<<<');
-          if(counterSoal==5){
-            let tidakIsiNama = true
-            while(tidakIsiNama){
-              nama = prompt("Masukkan nama: ")
-              if(nama){
-                tidakIsiNama = false;
-              };
-            };
-            
-            let scoreSebelum = localStorage.getItem("last-score");
 
-            if(scoreSebelum === null){
-              localStorage.setItem("nama", `${nama}`);
-            localStorage.setItem("last-score", `${score}`);
-            }else{
-              
-              localStorage.setItem("namaPenantang", `${nama}`);
-              localStorage.setItem("scorePenantang", `${score}`);
-
-              
-            }
-
-            
-            
-
-            window.location.href = 'index3.html'
-          }
-          gantiSoal();
-      };
-  });
-  
-  function pertama(){
+  function gantiSoal() {
     let perSoal = list[counterPertanyaan];
-    let {image,pertanyaan,pilihanJawaban} = perSoal;
-  
-    optionA.innerHTML = pilihanJawaban[0]
-    optionB.innerHTML = pilihanJawaban[1]
-    optionC.innerHTML = pilihanJawaban[2]
-    gambarBox.setAttribute("src", image)
+    let { image, pertanyaan, pilihanJawaban } = perSoal;
+
+    gambarBox.setAttribute("src", image);
+    optionA.innerHTML = pilihanJawaban[0];
+    optionB.innerHTML = pilihanJawaban[1];
+    optionC.innerHTML = pilihanJawaban[2];
     pertanyaanBox.innerHTML = pertanyaan;
-    gantiSoal()
+
+    if (counterPertanyaan == list.length - 1) {
+      counterPertanyaan = 0;
+    } else {
+      counterPertanyaan++;
+    }
+    counterSoal++;
   }
-  pertama()
-}else{
+
+  function cekJawaban(soal, userJawaban, list) {
+    for (let i = 0; i < list.length; i++) {
+      let perSoal = list[i];
+      // console.log(perSoal.jawabanBenar,userJawaban,'<<<<<')
+      if (perSoal.pertanyaan == soal) {
+        if (perSoal.jawabanBenar == userJawaban) {
+          score += 10;
+        }
+      }
+    }
+  }
+
+  let jawabanBox = document.getElementById("jawabanBox");
+  jawabanBox.addEventListener("click", function (el) {
+    if (
+      el.target.id == "optionA" ||
+      el.target.id == "optionB" ||
+      el.target.id == "optionC"
+    ) {
+      let jawaban = "";
+      let soal = pertanyaanBox.innerHTML;
+      if (el.target.id == "optionA") {
+        jawaban = optionA.innerHTML;
+      } else if (el.target.id == "optionB") {
+        jawaban = optionB.innerHTML;
+      } else if (el.target.id == "optionC") {
+        jawaban = optionC.innerHTML;
+      }
+      console.log(soal, jawaban);
+      cekJawaban(soal, jawaban, list);
+      console.log(score, counterSoal, "<<<<");
+      if (counterSoal == 5) {
+        let tidakIsiNama = true;
+        while (tidakIsiNama) {
+          nama = prompt("Masukkan nama: ");
+          if (nama) {
+            tidakIsiNama = false;
+          }
+        }
+
+        let scoreSebelum = localStorage.getItem("last-score");
+
+        if (scoreSebelum === null) {
+          localStorage.setItem("nama", `${nama}`);
+          localStorage.setItem("last-score", `${score}`);
+        } else {
+          localStorage.setItem("namaPenantang", `${nama}`);
+          localStorage.setItem("scorePenantang", `${score}`);
+        }
+
+        window.location.href = "index3.html";
+      }
+      gantiSoal();
+    }
+  });
+
+  function pertama() {
+    let perSoal = list[counterPertanyaan];
+    let { image, pertanyaan, pilihanJawaban } = perSoal;
+
+    optionA.innerHTML = pilihanJawaban[0];
+    optionB.innerHTML = pilihanJawaban[1];
+    optionC.innerHTML = pilihanJawaban[2];
+    gambarBox.setAttribute("src", image);
+    pertanyaanBox.innerHTML = pertanyaan;
+    gantiSoal();
+  }
+  pertama();
+} else {
   let scorePenantang = localStorage.getItem("scorePenantang");
   let namaPenantang = localStorage.getItem("namaPenantang");
   let lastScore = localStorage.getItem("last-score");
   let namaLastScore = localStorage.getItem("nama");
 
   console.log(scorePenantang === null);
-  console.log(Number(scorePenantang) > Number(lastScore), Number(scorePenantang), Number(lastScore));
+  console.log(
+    Number(scorePenantang) > Number(lastScore),
+    Number(scorePenantang),
+    Number(lastScore)
+  );
 
-  if(scorePenantang !== null){
-    if(Number(scorePenantang) > Number(lastScore)){
+  if (scorePenantang !== null) {
+    if (Number(scorePenantang) > Number(lastScore)) {
       document.getElementById("scorePlayer1").innerHTML = scorePenantang;
       document.getElementById("player1Score").innerHTML = namaPenantang;
       localStorage.setItem("nama", `${namaPenantang}`);
       localStorage.setItem("last-score", `${scorePenantang}`);
-    }else{
+    } else {
       document.getElementById("scorePlayer1").innerHTML = lastScore;
       document.getElementById("player1Score").innerHTML = namaLastScore;
     }
-  }else{
+  } else {
     document.getElementById("scorePlayer1").innerHTML = lastScore;
     document.getElementById("player1Score").innerHTML = namaLastScore;
   }
 
-  
-  
   const mainLagi = document.getElementById("playAgainButton");
-  mainLagi.addEventListener("click", function(){
-    window.location.href = 'index2.html'
-  })
+  mainLagi.addEventListener("click", function () {
+    window.location.href = "index2.html";
+  });
+
+  const kritikSaran = document.getElementById("kritikSaran");
+  kritikSaran.addEventListener("click", function () {
+    window.location.href = "index2.html";
+  });
 
   
 }
+
