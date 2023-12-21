ee8215bbceb79892c76bdcd2fa339b2755d542dd
balanced-B <ariobimo.mp@gmail.com>
2023-12-21 17:37:05 +0700

    lkas

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+
 let list = [
-    {image: 'https://asset-2.tstatic.net/tribunnews/foto/bank/images/thanos-dan-gauntlet-6-infinity-stones.jpg', pertanyaan: 'Kenapa Thanos tidak punya rambut?', jawaban: ['Supaya muat pake helm', 'Karena sudah takdir', 'Karena sudah sangat tua']},
-    {image: '', pertanyaan: 'Apa nama belakang Tony kalau sudah sakit?', jawaban: ['Tony Stroke', 'Tetap Tony Stark', '']},
-    {image: '', pertanyaan: '', jawaban: []},
-    {image: '', pertanyaan: '', jawaban: []},
-    {image: '', pertanyaan: '', jawaban: []},
+    {id: 1, image: '', pertanyaan: 'Kembang, kembang apa yang ngga mungkin dicium?', pilihanJawaban: ['kembang desa','kembang 7 rupa', 'kembang api'], jawabanBenar: 'kembang api'},
+    {id: 2, image: '', pertanyaan: 'Gimana cara biar anjing tidak kencing di jok depan?', pilihanJawaban: ['ngga usah ikut pergi', 'dipakein popok', 'pindahin ke jok depan'], jawabanBenar: 'pindahin ke jok depan'},
+    {id: 3, image: '', pertanyaan: 'Ketika kapal tenggelam atau pesawat jatuh, munculnya dimana?', pilihanJawaban: ['di dasar laut dan di daratan', 'jawaban atas dan bawah salah', 'di berita atau koran'], jawabanSalah2:'jawaban atas dan bawah salah', jawabanBenar: 'di berita atau koran'},
+    {id: 4, image: '', pertanyaan: 'Lebih berat mana, 100Kg kapas atau 1ooKg besi?', pilihanJawaban: ['besi lah bro', 'kapas sih kayaknya', 'sama-sama berat'], jawabanBenar: 'sama-sama berat'},
+    {id: 5, image: '', pertanyaan: 'sayur apa yang pintar nyanyi?', pilihanJawaban: ['karakter bawang putih', 'kol-aborasi', 'Kolplay'],  jawabanBenar: 'Kolplay'},
 ]
+
+function highScore(id) {
+    
+}
